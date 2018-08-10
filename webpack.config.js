3cc09cf6fb42d2c0f64c1e00f22da27e31055e84
Hiroki Takano <takano@hitokuse.com>
2018-08-10 10:40:01 +0900

    add sass-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,9 @@
 module.exports = {
   mode: 'production',
+  module: {
+    rules: [
+      { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+      { test: /\.scss$/, use: ['style-loader', 'css-loader', 'sass-loader'] },
+    ],
+  },
 };
